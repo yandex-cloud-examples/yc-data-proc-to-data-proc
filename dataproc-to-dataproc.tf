95e77a6517b68284d9312f7d1b02394e2a79c44d
Mikhail Darkhovskii <darkhovskii@yandex-team.ru>
2025-01-10 14:35:49 +0500

    Revert change to ssh path

--- dataproc-to-dataproc.tf
+++ dataproc-to-dataproc.tf
@@ -182,7 +182,7 @@ resource "yandex_dataproc_cluster" "dataproc-source-cluster" {
 
     hadoop {
       services        = ["SPARK", "YARN"]
-      ssh_public_keys = ["${file(local.dp_ssh_key)}"]
+      ssh_public_keys = [file(local.dp_ssh_key)]
       properties = {
         # For running PySpark jobs when Yandex Data Processing is integrated with Metastore
         "spark:spark.sql.hive.metastore.sharedPrefixes" = "com.amazonaws,ru.yandex.cloud"
